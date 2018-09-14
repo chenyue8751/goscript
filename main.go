02c7a2c0ae585bd2b66366fc4faf671b61bebbac
chenyuejun <chenyuejun@qiyi.com>
2018-09-14 23:12:59 +0800

    modify

--- main.go
+++ main.go
@@ -3,77 +3,78 @@ package main
 import (
 	"flag"
 	"fmt"
-    "os"
 	"goscript/config"
 	"goscript/internal"
 	"goscript/model"
 	"goscript/redisModel"
+	"os"
 	"time"
 )
 
 var (
-    h bool
-    command string
-    date string
+	h       bool
+	command string
+	date    string
 )
 
 func init() {
-    flag.BoolVar(&h, "h", false, "this script's usage")
+	flag.BoolVar(&h, "h", false, "this script's usage")
 	flag.StringVar(&command, "command", "", "set command: clean_battle,recover_battle")
-    flag.StringVar(&date, "date", "", "simulate date: 2000-01-01")
-    flag.Usage = Usage
+	flag.StringVar(&date, "date", "", "simulate date: 2000-01-01")
+	flag.Usage = Usage
 }
 
 func main() {
 	flag.Parse()
 
-    if h {
-        flag.Usage()
-    }
+	if h {
+		flag.Usage()
+	}
 
 	configs := config.Config()
 	db := configs.Database
 	model.InitDB(db.Host, db.Port, db.Dbname, db.Username, db.Password)
 	redisModel.InitRedis(configs.Redis.Server, configs.Redis.Password)
 
-    switch command {
-    case "clean_battle":
-        cleanBattle(date)
-    case "recover_battle":
-        recoverBattle(date)
-    default:
-        flag.Usage()
-    }
+	switch command {
+	case "clean_battle":
+		cleanBattle(date)
+	case "recover_battle":
+		recoverBattle(date)
+	default:
+		flag.Usage()
+	}
 }
 
 func Usage() {
-    fmt.Fprintf(os.Stderr, `Minigame system script
+	fmt.Fprintf(os.Stderr, `Minigame system script
 Version: v1.0
 Usage: goscript [-h] [-command=clean_battle]
 Options:
 `)
-    flag.PrintDefaults()
+	flag.PrintDefaults()
 }
 
 func cleanBattle(handleDate string) {
-    var now time.Time
-    if handleDate == "" {
-        now = time.Now()
-    } else {
-        now, _ = time.Parse("2006-01-02", handleDate)
-    }
+	var now time.Time
+	if handleDate == "" {
+		now = time.Now()
+	} else {
+		now, _ = time.Parse("2006-01-02", handleDate)
+	}
 	lastMonday := internal.LastMonday(now).Format("2006-01-02")
 	count, _ := redisModel.CleanBattle(lastMonday)
-	fmt.Println("delete keys, nums: ", count)
+
+	fmt.Println("Delete battle keys success, nums: ", count)
 }
 
-func recoverBattle(handleDate string) bool {
-    var now time.Time
-    if handleDate == "" {
-        now = time.Now()
-    } else {
-        now, _ = time.Parse(time.RFC3339, handleDate)
-    }
+func recoverBattle(handleDate string) {
+	var now time.Time
+	if handleDate == "" {
+		now = time.Now()
+	} else {
+		now, _ = time.Parse(time.RFC3339, handleDate)
+	}
 	date := internal.ThisMonday(now).Format("2006-01-02")
 	record := model.BattlePlays(date)
 	data := make(map[int]map[int]map[int]int)
@@ -94,5 +95,5 @@ func recoverBattle(handleDate string) bool {
 
 	redisModel.InitBattle(data, date)
 
-	return true
+	fmt.Println("Recover battle date success.")
 }
